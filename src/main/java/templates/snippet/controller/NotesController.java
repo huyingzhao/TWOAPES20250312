aee5c377dcb0397cdffff4ab531e392c4d522021
huyingzhao <13824314620@139.com>
2025-03-12 22:17:28 +0800

    update

--- src/main/java/templates/snippet/controller/NotesController.java
+++ src/main/java/templates/snippet/controller/NotesController.java
@@ -3,9 +3,8 @@ package templates.snippet.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import templates.page.PageDomain;
 import templates.snippet.doman.SnippetCode;
 import templates.snippet.mapper.ISnippetCodeMapper;
@@ -15,21 +14,20 @@ import java.util.List;
 /**
  * @author add by huyingzhao 2025-02-19 23:00
  */
-@Controller
-@RequestMapping("/notes")
+@RestController
 public class NotesController {
     @Autowired
     private ISnippetCodeMapper snippetCodeMapper;
 
-    @GetMapping("/notes")
-    public String notes(ModelMap map,@RequestParam(required = false) String title, @RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "8") int pageSize) {
+    @RequestMapping(value = "/notes/notes", method = {RequestMethod.GET, RequestMethod.POST})
+    public ModelAndView  notes(ModelMap map, @RequestParam(required = false) String title, @RequestParam(defaultValue = "1") int pageNum, @RequestParam(defaultValue = "8") int pageSize) {
         SnippetCode pageDomain = new SnippetCode();
         pageDomain.setTitle(title);
         int countTotalSnippets = snippetCodeMapper.countTotalSnippets(pageDomain);
         int totalPages = (int) Math.ceil((double) countTotalSnippets / pageSize);
 
         pageNum = Math.max(1, Math.min(pageNum, totalPages)); // Ensure pageNum is within bounds
-        pageDomain.setPageNum((pageNum-1)*pageSize);
+        pageDomain.setPageNum((pageNum - 1) * pageSize);
         pageDomain.setPageSize(pageSize);
 
         List<SnippetCode> notes = snippetCodeMapper.selectSnippetWithPagination(pageDomain);
@@ -39,6 +37,6 @@ public class NotesController {
         map.put("countTotalSnippets", countTotalSnippets);
         map.put("totalPages", totalPages);
 
-        return "notes/notes";
+        return new ModelAndView("notes/notes");
     }
 }
